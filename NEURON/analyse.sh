7e14eaf5bfb3bdb8da99152295247ca3f959b5cb
Padraig Gleeson <p.gleeson@gmail.com>
2015-11-03 10:28:31 +0000

    Tidied up mods

--- NEURON/analyse.sh
+++ NEURON/analyse.sh
@@ -1,4 +1,4 @@
 set -e
 pynml-modchananalysis kdrmt -stepV 5 -temperature [28,35] -nogui
 pynml-modchananalysis kamt -stepV 5 -temperature [28,35] -nogui
-pynml-modchananalysis nax -stepV 5 -temperature [28,35] -modFile naxn.mod -nogui
+pynml-modchananalysis nax -stepV 5 -temperature [28,35] -modFile naxn.mod -dt 0.0025 -nogui
